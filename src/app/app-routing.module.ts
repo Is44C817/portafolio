30cc4d3000f4f594a8a96cf0e509a603f8b626d6
Is44C817 <davidisaac.sanchez.contractor@bbva.com>
2024-04-22 15:02:46 -0600

    redireccionamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ComunicadosComponent } from './components/comunicados/comunicados.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'portafolio', pathMatch: 'full' },
+  { path: 'portafolio', component: ComunicadosComponent },
+  { path: '**', redirectTo: 'portafolio', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
